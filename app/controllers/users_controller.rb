4dbf868986a2b939db853161df8d5d4461e80d29
Bekassyl <bmukazhan@gmail.com>
2017-06-16 11:18:45 +0600

    Edit delete show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,39 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :destroy, :edit, :update]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+    if	@user.update_attributes(user_params)
+			redirect_to	@user
+		else
+			render	"edit"
+		end
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(params[ :user])
+    @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to @user
     else
       render "new"
     end
@@ -17,6 +43,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:login, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
+  root "users#new"
   get "/about", to: "static_pages#about"
-  get "/signup", to: "users#new"
+  get "/method.kz", to: "static_pages#method"
+  
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
